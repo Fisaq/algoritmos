dbaff81bd3527f48228373a418bdb639f84e3e9c
Isaque Ferreira <95030412+Fisaq@users.noreply.github.com>
2023-12-27 14:34:43 -0300

    Create ex08.c

---
+++ src/iniciante_2/ex08.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+int distanciaPontos(int x1, int y1, int x2, int y2){
+
+  printf("\nPonto A: (%d,%d)",x1,y1);
+  printf("\nPonto B: (%d,%d)",x2,y2);
+
+  int sub_x = x2 - x1;
+  int sub_y = y2 - y1;
+
+  int sub_x_quad = pow(sub_x,2);
+  int sub_y_quad = pow(sub_y,2);
+
+  int soma_quad = sub_x_quad + sub_y_quad;
+
+  double distancia = sqrt(soma_quad);
+
+  printf("\n\nA distãncia entre A e B é %.2lf",distancia);
+  
+  return 0;
+}
+
+int exI208(){
+
+  distanciaPontos(1, 2, 7, 8);
+  
+  return 0;
+}
