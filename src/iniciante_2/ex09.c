4f3c1f65ede139dac221ba7eb8f8149c61396903
Isaque Ferreira <95030412+Fisaq@users.noreply.github.com>
2023-12-27 14:35:05 -0300

    Create ex09.c

---
+++ src/iniciante_2/ex09.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int tabelaASCII(int num){
+  
+ printf("\nO caractere correspondente a %d na Tabela ASCII é: %c", num, num);
+  
+  return 0;
+}
+
+int exI209(){
+
+  tabelaASCII(67);
+
+  //Saída:
+
+  //O caractere correspondente a 67 na Tabela ASCII é: C
+  
+  return 0;
+}
