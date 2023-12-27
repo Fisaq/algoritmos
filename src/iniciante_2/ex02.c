a1fe219d4ff098064e04abf40223dc1c7d5f5a11
Isaque Ferreira <95030412+Fisaq@users.noreply.github.com>
2023-12-27 14:30:27 -0300

    Create ex02.c

---
+++ src/iniciante_2/ex02.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int resQuo(int a, int b){
+
+  int quociente = a / b;
+  int resto = a % b;
+
+  printf("\nA: %d", a);
+  printf("\nB: %d", b);
+  printf("\n\nQuociente: %d", quociente);
+  printf("\nResto: %d", resto);
+  
+  return 0;
+}
+
+int exI202(){
+
+  resQuo(22, 5);
+  
+  return 0;
+}
