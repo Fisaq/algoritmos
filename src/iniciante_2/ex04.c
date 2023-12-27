8b89a77272738a7eeb8909705c3313eca1d1c30e
Isaque Ferreira <95030412+Fisaq@users.noreply.github.com>
2023-12-27 14:31:11 -0300

    Create ex04.c

---
+++ src/iniciante_2/ex04.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define HECTARE 10000
+
+int compTerreno(int largura, int comprimento){
+
+  int area_mq = largura * comprimento;
+  double area_hec = area_mq/HECTARE;
+
+  if(largura < 100 || comprimento < 100){
+    printf("\nFavor, informar uma altura e comprimento maiores que 100m");
+  }
+  else{
+    printf("\nLargura do Terreno: %dm", largura);
+    printf("\nComprimento do Terreno: %dm", comprimento);
+    printf("\n\nÁrea: %dm²", area_mq);
+    printf("\nÁrea em hectares: %.lf hectares", area_hec);
+  }
+  
+  return 0;
+}
+
+int exI204(){
+
+  compTerreno(99, 100);
+  return 0;
+}
